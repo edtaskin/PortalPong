f69f9f8adb05f4ff1533deb29771c2a0004b6206
edtaskin <91011207+edtaskin@users.noreply.github.com>
2023-06-27 00:10:21 +0300

    file names fixed

---
+++ src/Button.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Button:
+    buttons = []
+
+    def __init__(self, display_screen, content, rect):
+        self.content = content
+        self.display_screen = display_screen
+        self.rect = rect
+        self.is_pressed = False
+        Button.buttons.append(self)
+        
+    @classmethod
+    def from_image(cls, display_screen, img, rect):
+        return cls(display_screen, img, rect)
+
+    @classmethod
+    def from_text(cls, display_screen, txt, font, rect):
+        return cls(display_screen, font.render(txt, False, "white"), rect)
